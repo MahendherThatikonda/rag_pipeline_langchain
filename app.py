4b939d65225e8524ff3006d00df2aba9b20ad687
MahendherThatikonda <116361201+MahendherThatikonda@users.noreply.github.com>
2025-12-07 19:40:36 +1300

    Initial clean commit (no secrets)

---
+++ app.py
@@ -0,0 +1,17 @@
+from SRCe.data_loader import load_all_documents
+from SRCe.vectorstore import FaissVectorStore
+from SRCe.search import RAGSearch
+
+
+# Example usage
+if __name__ == "__main__":
+    
+    docs = load_all_documents("data")
+    store = FaissVectorStore("faiss_store")
+    #store.build_from_documents(docs)
+    store.load()
+    #print(store.query("What is attention mechanism?", top_k=3))
+    rag_search = RAGSearch()
+    query = "What is attention mechanism?"
+    summary = rag_search.search_and_summarize(query, top_k=3)
+    print("Summary:", summary)
